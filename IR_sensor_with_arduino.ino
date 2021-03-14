7171467bf6a6c799a4ad0d11bcd07f16ed3473d5
ainn1234 <77329942+ainn1234@users.noreply.github.com>
2021-03-14 23:15:44 +0500

    Add files via upload

---
+++ IR_sensor_with_arduino.ino
@@ -0,0 +1,30 @@
+int IRSensor = 2; // connect ir sensor to arduino pin 2
+int LED = 13; // conect Led to arduino pin 13
+
+
+
+void setup() 
+{
+
+
+
+  pinMode (IRSensor, INPUT); // sensor pin INPUT
+  pinMode (LED, OUTPUT); // Led pin OUTPUT
+}
+
+void loop()
+{
+  int statusSensor = digitalRead (IRSensor);
+  
+  if (statusSensor == 1)
+    digitalWrite(LED, LOW); // LED LOW
+    Serial.println("Door close");
+  }
+  
+  else
+  {
+    digitalWrite(LED, HIGH); // LED High
+    Serial.println("Door open");
+  }
+  
+}
